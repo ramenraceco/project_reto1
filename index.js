fe3126c81f2c44802234893c416114c4d0998752
lizzmar <lizzmar.idol@gmail.com>
2018-07-09 14:45:06 -0400

    segunda modifiacion al index

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ db.sequelize.sync().done(() => {
 app.listen(app.get('port'),() =>{
     console.log('Server on port', app.get('port'));    
 });
+ // segunda modificacion de ramenraceco
 
+const db = require('./database');
+db.sequelize.sync().done(() => {
+   console.log("\n***Base de datos generada***");
+});
+//Server is listening
+//get: obtener algo recibe algo
+app.listen(app.get('port'),() =>{
+    console.log('Server on port', app.get('port'));    
+});
